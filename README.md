c195d330f49b58da39faea974d23faaf8e276fb9
Vasu7389 <awasthi.vasu65@gmail.com>
2024-06-14 20:01:34 +0530

    nodejs script to download windows lockscreen spotlight image to local folder

---
+++ README.md
@@ -0,0 +1,5 @@
+Step to run -
+
+1. Open terminal from this file location
+2. Execute - "node download-windows-lockscreen-images.js"
+3. Checkout "C:\Users\<your_username>\Pictures\Spotlight Images" folder in Windows.
---
+++ download-windows-lockscreen-images.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+
+// Source directory where Spotlight images are stored
+const spotlightDir = path.join(
+  process.env.USERPROFILE,
+  "AppData",
+  "Local",
+  "Packages",
+  "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy",
+  "LocalState",
+  "Assets"
+);
+
+// Destination directory where images will be copied to
+const destDir = path.join(
+  process.env.USERPROFILE,
+  "Pictures",
+  "Spotlight Images"
+);
+
+// Create the destination directory if it doesn't exist
+if (!fs.existsSync(destDir)) {
+  fs.mkdirSync(destDir, { recursive: true });
+}
+
+fs.readdir(spotlightDir, (err, files) => {
+  if (err) {
+    console.error("Error reading Spotlight directory:", err);
+    return;
+  }
+
+  files.forEach((file) => {
+    const filePath = path.join(spotlightDir, file);
+
+    // Filter out small files (not wallpapers)
+    if (fs.statSync(filePath).size > 100000) {
+      const destPath = path.join(destDir, file + ".jpg");
+
+      // Copy file to the destination directory with a .jpg extension
+      fs.copyFile(filePath, destPath, (err) => {
+        if (err) {
+          console.error("Error copying file:", err);
+        } else {
+          console.log(`Copied ${filePath} to ${destPath}`);
+        }
+      });
+    }
+  });
+
+  console.log(`All Spotlight images have been copied to ${destDir}`);
+});
